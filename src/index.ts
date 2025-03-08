ddbc447bd11c3ba7d6c03c9d8836e7303cd6a3e0
vinsmokejazz <vinsmokejazz@gmail.com>
2025-03-08 14:41:38 +0530

    minor error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { ContentModel, LinkModel, UserModel } from "./db";
 import bcrypt from "bcrypt";
@@ -6,137 +6,143 @@ import { JWT_USER_PASSWORD } from "./config";
 import { userMiddleware } from "./middleware";
 import { random } from "./utils";
 
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+    }
+  }
+}
 
 const app = express();
 app.use(express.json());
 
-app.post("/api/v1/signup", async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
+// Signup Route
+app.post("/api/v1/signup", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    await UserModel.create({ username: username, password: hashedPassword });
-    res.status(201).json("signed up successfully");
+    await UserModel.create({ username, password: hashedPassword });
+    res.status(201).json("Signed up successfully");
   } catch (error) {
-    res.status(400).json("user already exists")
+    res.status(409).json("User already exists");
   }
 });
 
+// Signin Route
+//@ts-ignore
+app.post("/api/v1/signin", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const user:any = await UserModel.findOne({ username });
 
-app.post("/api/v1/signin", async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  const user = await UserModel.findOne({ username });
   if (!user) {
-    res.status(403).json("incorrect creds");
+    return res.status(403).json("Incorrect credentials");
   }
-  //@ts-ignore
+
   const passwordMatch = await bcrypt.compare(password, user.password);
-
-  if (passwordMatch) {
-    //@ts-ignore
-    const token = jwt.sign({ id: user._id.toString() }, JWT_USER_PASSWORD);
-    res.status(200).json({
-      token: token,
-    })
-  } else {
-    res.status(403).json("incorrect creds");
+  if (!passwordMatch) {
+    return res.status(403).json("Incorrect credentials");
   }
-})
 
-app.post("/api/v1/content", userMiddleware, async (req, res) => {
-  const link = req.body.link;
-  const type = req.body.type;
-  await ContentModel.create({
-    link,
-    type,
-    //@ts-ignore
-    userId: req.userId,
-    tags: []
+  const token = jwt.sign(
+    { id: user._id.toString() },
+    JWT_USER_PASSWORD as string
+  );
+  res.json({ token });
+});
 
-  })
-
-})
-
-app.get("/api/v1/content", userMiddleware, async (req, res) => {
-  //@ts-ignore
-  const userId = req.userId;
-  const content = await ContentModel.find({
-    userId: userId
-  }).populate("userId", "username")
-  res.json({
-    content
-  })
-
-})
-
-app.delete("/api/v1/content", userMiddleware, async (req, res) => {
-  const contentId = req.body.contentId;
-  await ContentModel.deleteMany({
-    contentId,
-    //@ts-ignore
-    userId: req.userId
-  })
-  res.json("Deleted")
-
-
-})
-
-app.post("/api/v1/brain/:share", userMiddleware, async (req, res) => {
-  const share = req.body.share;
-  if (share) {
-    const existingLink = await LinkModel.findOne({
-      //@ts-ignore
-      userId: req.userId
+// Content Routes
+app.post("/api/v1/content", userMiddleware, async (req: Request, res: Response) => {
+  const { link, type } = req.body;
+  try {
+    const content = await ContentModel.create({
+      link,
+      type,
+      userId: req.userId,
+      tags: []
     });
-    if (existingLink) {
-      res.json({
-        hash: existingLink.hash
-      })
-      return;
-    }
-    const hash = random(10);
-    await LinkModel.create({
-      hash: hash
-    })
-  } else {
-    await LinkModel.deleteOne({
-      //@ts-ignore
-      userId: req.userId
-    });
-    res.json("removed link");
+    res.status(201).json(content);
+  } catch (error) {
+    res.status(500).json("Error creating content");
   }
 });
 
-app.get("/api/v1/brain/:shareLink", async (req, res) => {
-  const hash = req.params.shareLink;
-  const link = await LinkModel.findOne({
-    hash
-  });
-
-  if (!link) {
-    res.status(411).json(" sorry incorrect input");
-    return;
+app.get("/api/v1/content", userMiddleware, async (req: Request, res: Response) => {
+  try {
+    const content = await ContentModel.find({ userId: req.userId })
+      .populate("userId", "username");
+    res.json({ content });
+  } catch (error) {
+    res.status(500).json("Error fetching content");
   }
-  const content = await ContentModel.find({
-    userId: link.userId
-  })
-  const user = await UserModel.findOne({
-    _id: link.userId
-  })
+});
 
-  if (!user) {
-    res.status(411).json("user not found");
-    return;
+app.delete("/api/v1/content", userMiddleware, async (req: Request, res: Response) => {
+  const { contentId } = req.body;
+  try {
+    await ContentModel.deleteOne({ _id: contentId, userId: req.userId });
+    res.json("Content deleted successfully");
+  } catch (error) {
+    res.status(500).json("Error deleting content");
   }
-  res.json({
-    username: user.username,
-    content: content
-  })
-})
+});
 
+// Sharing Routes 
+//@ts-ignore
+app.post("/api/v1/brain/:share", userMiddleware, async (req: Request, res: Response) => {
+  const share = req.params.share === "true";
+  
+  if (share) {
+    try {
+      const existingLink = await LinkModel.findOne({ userId: req.userId });
+      if (existingLink) {
+        return res.json({ hash: existingLink.hash });
+      }
 
-app.listen(3000);
+      const hash = random(10);
+      await LinkModel.create({ hash, userId: req.userId });
+      res.json({ hash });
+    } catch (error) {
+      res.status(500).json("Error creating share link");
+    }
+  } else {
+    try {
+      await LinkModel.deleteOne({ userId: req.userId });
+      res.json("Share link removed");
+    } catch (error) {
+      res.status(500).json("Error removing share link");
+    }
+  }
+});
 
+//@ts-ignore
+app.get("/api/v1/brain/:shareLink", async (req: Request, res: Response) => {
+  const { shareLink } = req.params;
+  try {
+    const link = await LinkModel.findOne({ hash: shareLink });
+    if (!link || !link.userId) {
+      return res.status(404).json("Invalid share link");
+    }
+
+    const [content, user] = await Promise.all([
+      ContentModel.find({ userId: link.userId }),
+      UserModel.findById(link.userId)
+    ]);
+
+    if (!user) {
+      return res.status(404).json("User not found");
+    }
+
+    res.json({
+      username: user.username,
+      content
+    });
+  } catch (error) {
+    res.status(500).json("Error retrieving shared content");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
